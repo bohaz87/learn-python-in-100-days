b25be6276f3ff986356e52dbf47a90370bb66c89
Bob Zhong <sbob@qq.com>
2024-06-17 22:47:12 +0800

    start learning

---
+++ day-002/hello world.py
@@ -0,0 +1,5 @@
+"""
+this is my first python program
+"""
+# print('Hello World!')
+print('Goodbye World!')
---
+++ day-003/types.py
@@ -0,0 +1,17 @@
+# int
+print(0b100)
+print(0o100)
+print(100)
+print(0x100)
+
+# float
+print(123.456)
+print(1.23456e3)
+
+# string
+print('hello') # 单引号
+print("hello") # 双引号
+
+# bool
+print(True)
+print(False)
